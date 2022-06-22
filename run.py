38f6f487312ccbc134ac635ac800137c3bb7ad0f
Martin Krook <krookmartin@yahoo.com>
2022-06-22 21:07:19 +0000

    changed menu, work in progress

--- run.py
+++ run.py
@@ -4,53 +4,53 @@
 import random
 
 def menu():
-    print('[0] Rock/Paper/Scissors')
-    print('[1] option 2')
-    print('[2] option 3')
+    print('[1] Rock/Paper/Scissors')
+    print('[2] option 2')
+    print('[3] option 3')
+    print('[0] end program')
 
 
 menu()
 
 
-option = int(input('Enter the number of the game you want to play'))
+option = int(input('Enter the number of the game you want to play '))
+while option != 0:
+    if option == 1:
 
-if option == 0:
+        player_score = 0
+        computer_score = 0
+        choices = ['rock', 'paper', 'scissors']
 
-    player_score = 0
-    computer_score = 0
-    choices = ['rock', 'paper', 'scissors']
+        while True:
+            p_choice = input('Rock/Paper/Scissors or E to end: ').lower()
+            if p_choice == "e":
+                break
+            if p_choice not in choices:
+                continue
 
-    while True:
-        p_choice = input('Rock/Paper/Scissors or E to end' + ':').lower()
-        if p_choice == "e":
-            break
-        if p_choice not in choices:
-            continue
+            random_choice = random.randint(0, 2)
 
-        random_choice = random.randint(0, 2)
+            computer_choice = choices[random_choice]
+            print("The computer's choice was", computer_choice)
 
-        computer_choice = choices[random_choice]
-        print("The computer's choice was", computer_choice)
+            if p_choice == 'rock' and computer_choice == 'scissors':
+                print('Plyers wins!')
+                player_score += 1
 
-        if p_choice == 'rock' and computer_choice == 'scissors':
-            print('Plyers wins!')
-            player_score += 1
-
-        elif p_choice == 'paper' and computer_choice == 'rock':
-            print('Plyers wins!')
-            player_score += 1
-           
-        elif p_choice == 'scissors' and computer_choice == 'rock':
-            print('Plyers wins!')
-            player_score += 1
-  
-        else:
-            print('You lost')
-            computer_score += 1
-        
-    print('Final score player:', player_score)
-    print('Final score computer:', computer_score)
-    print('Thank you for playing, have a nice day!')
-    menu()
-
+            elif p_choice == 'paper' and computer_choice == 'rock':
+                print('Plyers wins!')
+                player_score += 1
+            
+            elif p_choice == 'scissors' and computer_choice == 'rock':
+                print('Plyers wins!')
+                player_score += 1
+    
+            else:
+                print('You lost')
+                computer_score += 1
+            
+        print('Final score player:', player_score)
+        print('Final score computer:', computer_score)
+        print('Thank you for playing, have a nice day!')
+        menu()
 #elif option == 1:
