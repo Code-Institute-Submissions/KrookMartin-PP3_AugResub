22097128f3c2391e2a696d986f21c34cb6b5713f
Martin Krook <krookmartin@yahoo.com>
2022-06-28 07:29:28 +0000

    add while loop to rpsgame to fix correct score

--- run.py
+++ run.py
@@ -5,93 +5,97 @@ import random
 
 
 def rpsgame():
+    """randomly picks rock paper scissors againt player"""
+   
     player_score = 0
     computer_score = 0
     choices = ['rock', 'paper', 'scissors']
 
-    p_choice = input('Rock/Paper/Scissors or E to end: ').lower()
-    if p_choice == "e":
-        print('Final score player:', player_score)
-        print('Final score computer:', computer_score)
-        print('Thank you for playing, have a nice day!')
-        menu()
+    while True:
+        p_choice = input('Rock/Paper/Scissors or E to end: ').lower()
+        if p_choice == "e":
+            break
 
-    elif p_choice in choices:
+        if p_choice not in choices:
+            continue
+        
         random_choice = random.randint(0, 2)
-
         computer_choice = choices[random_choice]
         print("The computer's choice was", computer_choice)
 
         if p_choice == 'rock' and computer_choice == 'scissors':
-            print('Plyers wins!')
+            print('Player wins!')
             player_score += 1
 
         elif p_choice == 'paper' and computer_choice == 'rock':
-            print('Plyers wins!')
+            print('Player wins!')
             player_score += 1
-
+                
         elif p_choice == 'scissors' and computer_choice == 'rock':
-            print('Plyers wins!')
+            print('Player wins!')
             player_score += 1
 
         else:
             print('You lost')
             computer_score += 1
-    else:
-        print('Please enter rock/papper or scissor')
-
+    
+    print('Final score player:' + str(player_score))
+    print('Final score computer:' + str(computer_score))
+    print('Thank you for playing, have a nice day!')
+    menu()
+    
 
 def quiz():
+    """Quiz game that ask for user inputs in form of answers"""
     print('Welcome to know your Nintendo')
     start_game = input('Start game? yes/no? ')
-    player_score = 0
+    score = 0
     if start_game.lower() != 'yes':
         print('goodbye!')
         menu()
     else:
         print('Game on!')
-        player_score = 0
-
+        
         question = input('Whats the name of Super Marios brother? ')
         if question.lower() == 'luigi':
-            player_score += 1
+            score += 1
             print('Correct')
-        else: 
+        else:
             print('Incorrect, the correct answer is luigi')
 
         question = input('What is the name of the protagonist in Metal Gear? ')
         if question.lower() == 'snake':
-            player_score += 1
+            score += 1
             print('Correct')
-        else: 
+        else:
             print('Incorrect, the correct answer is snake')
 
         question = input('What is the name of Super Marios nemesis? ')
         if question.lower() == 'bowser':
-            player_score += 1
+            score += 1
             print('Correct')
-        else: 
-            print('Incorrect, the correct answer is bowser')    
+        else:
+            print('Incorrect, the correct answer is bowser')
 
         question = input('What is the name of the protagonist in Zelda? ')
         if question.lower() == 'link':
-            player_score += 1
+            score += 1
             print('Correct')
         else:
             print('Incorrect, the correct answer is link')
-        
+ 
         question = input('What color is the original megaman? ')
         if question.lower() == 'blue':
-            player_score += 1
+            score += 1
             print('Correct')
         else:
             print('Incorrect, the correct answer is blue')
 
-    print('Thanks for playing you got ' + str(player_score) + 'points')
+    print('Thanks for playing you got ' + str(score) + 'points')
 
 
 def magic_ball():
-    answer = ['Yes!', 'No', 'Maybe', 'Think about it i must', 'It depends'] 
+    answer = ['Yes!', 'No', 'Maybe', 'Think about it i must', 'It depends']
 
     while str(input('Ask the Magic 8Ball? yes or no ')).lower() != 'no':
         str(input('What is your question? '))
